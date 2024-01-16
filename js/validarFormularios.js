eaef733107ddfd963c3490c5b3522239125d631d
Patricio Gallo <prgasesor@gmail.com>
2024-01-16 16:17:35 -0300

    Aplicando correcciones JavaScript

--- js/validarFormularios.js
+++ js/validarFormularios.js
@@ -1,55 +1,55 @@
-const nombre = document.getElementById("nombre");
-const mail = document.getElementById("mail");
-const tel = document.getElementById("tel");
-const tipoDeSeguro = document.getElementById("tipoDeSeguro");
-const comentarios = document.getElementById("comentarios");
-const preferenciaHora = document.getElementById("preferenciaHora");
-const preferenciaContacto = document.getElementById("preferenciaContacto");
-const mensaje = document.getElementById("mensaje");
-const fecha = document.getElementById("fecha");
-const hora = document.getElementById("hora");
-const lugar = document.getElementById("lugar");
-const formaOcurrencia = document.getElementById("formaOcurrencia");
-const cedula = document.getElementById("cedula");
-const carnetManejo = document.getElementById("carnetManejo");
-const fotosAuto = document.getElementById("fotosAuto");
-const patenteTercero = document.getElementById("patenteTercero");
-const aseguradoraTercero = document.getElementById("aseguradoraTercero");
-const polizaTercero = document.getElementById("polizaTercero");
-const conductorTercero = document.getElementById("conductorTercero");
-const actividad = document.getElementById("actividad");
-const cantidadPersonasAp = document.getElementById("cantidadPersonasAp");
-const provincia = document.getElementById("provincia");
-const modelo = document.getElementById("modelo");
-const year = document.getElementById("year");
-const marca = document.getElementById("marca");
+const formularioData = {
+    nombre: document.getElementById("nombre"),
+    mail: document.getElementById("mail"),
+    tel: document.getElementById("tel"),
+    tipoDeSeguro: document.getElementById("tipoDeSeguro"),
+    comentarios: document.getElementById("comentarios"),
+    preferenciaHora: document.getElementById("preferenciaHora"),
+    preferenciaContacto: document.getElementById("preferenciaContacto"),
+    mensaje: document.getElementById("mensaje"),
+    fecha: document.getElementById("fecha"),
+    hora: document.getElementById("hora"),
+    lugar: document.getElementById("lugar"),
+    formaOcurrencia: document.getElementById("formaOcurrencia"),
+    cedula: document.getElementById("cedula"),
+    carnetManejo: document.getElementById("carnetManejo"),
+    fotosAuto: document.getElementById("fotosAuto"),
+    patenteTercero: document.getElementById("patenteTercero"),
+    aseguradoraTercero: document.getElementById("aseguradoraTercero"),
+    polizaTercero: document.getElementById("polizaTercero"),
+    conductorTercero: document.getElementById("conductorTercero"),
+    actividad: document.getElementById("actividad"),
+    cantidadPersonasAp: document.getElementById("cantidadPersonasAp"),
+    provincia: document.getElementById("provincia"),
+    modelo: document.getElementById("modelo"),
+    year: document.getElementById("year"),
+    marca: document.getElementById("marca"),
+};
 
-const formularioVidaCapitalizacion = document.getElementById("formularioVidaCapitalizacion");
+const regexMarca = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexTel = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
 const formularioSobreVos = document.getElementById("formularioSobreVos");
-const formularioSeguroAutomotor = document.getElementById("formularioSeguroAutomotor");
-const formularioSiniestros = document.getElementById("formularioSiniestros");
-const formularioContacto = document.getElementById("formularioContacto");
-const formularioReunion = document.getElementById("formularioReunion");
+const parrafo = document.getElementById("warnings");
 
-const parrafo = document.getElementById("warnings")
+function validarCampo(campo, regex, mensajeError) {
+    if (!regex.test(campo.value)) {
+        return mensajeError;
+    }
+    return "";
+}
 
-formularioSobreVos.addEventListener("submit", e => {
+function validarFormulario(data) {
+    let warnings = "";
+    warnings += validarCampo(data.marca, regexMarca, "La marca no es válida <br>");
+    warnings += validarCampo(data.tel, regexTel, "El teléfono no es válido <br>");
+    warnings += validarCampo(data.mail, regexEmail, "El mail no es válido <br>");
+
+    parrafo.innerHTML = warnings;
+}
+
+formularioSobreVos.addEventListener("submit", (e) => {
     e.preventDefault();
-    let warnings = ""
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    let regexTel = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
-    let entrar = false
-    if (!regexTel.test(tel.value)) {
-        warnings += `El teléfono no es válido <br>`
-        entrar = true
-    }
-    console.log(regexEmail.test(mail.value))
-    if (!regexEmail.test(mail.value)) {
-        warnings += `El mail no es válido <br>`
-        entrar = true
-    }
-
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }
-})
+    validarFormulario(formularioData);
+});
